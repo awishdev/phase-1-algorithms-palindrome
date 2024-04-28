126d9539de1f134424183ae61d16149905a265a6
Arthur Ish <arthurishdev@gmail.com>
2024-04-27 22:33:49 -0700

    tacocat is true

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  var stringArr = word.split("");
+  stringArr = stringArr.reverse();  
+  var newWord = stringArr.join("");
+  
+  return word === newWord;
+
 }
 
 /* 
   Add your pseudocode here
+  take string
+
+  split into array
+  reverse array
+  join into string
+  compare to original string
+  return result
+
+
 */
 
 /*
   Add written explanation of your solution here
+  reverse the input string to compare
 */
 
 // You can run `node index.js` to view these console logs
